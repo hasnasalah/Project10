4c1dc2d4255a27c994bb489e7228604d9233d85c
hasna <hasna@LAPTOP-S7AIGBHT>
2022-11-29 19:56:39 -0500

    almost

--- Source.cpp
+++ Source.cpp
@@ -2,8 +2,41 @@
 //11/29/2022
 // cis1202
 #include<iostream>
+using namespace std;
+
+class invalidCharacterExeption{};
+class invalidRangeExeption {};
+// function prototype
+
+char character(char , int );
+
+
+
 
 
 int main() {
-	cout<<"character
+	cout << "character ('a'-1)" << endl;
+	try
+	{
+		character('a', -1);
+	}
+	catch(invalidRangeExeption E) {
+		cout << "Error! invalid character exeption" << endl;
+
+	}
+	try{}
+	catch()
+}
+
+char character(char start, int offset) {
+	int num;
+	if ((start < 65 || start>90) && (start < 97 || start>122)) 
+		throw invalidCharacterExeption();
+
+		num = start + offset;
+
+		if( (num < 65 || num>90) && (num < 97 || num>122)) 
+		throw invalidCharacterExeption{};
+		return static_cast<char>(num);
+
 }
